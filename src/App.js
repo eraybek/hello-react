1a355c7e04eeaa4ebd676c35654227796b503fb7
Eray Aybek <aybek17@itu.edu.tr>
2022-11-09 15:03:17 +0300

    update js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Header from "./components/Header"
+// import Login from "./components/Login"
+import User from "./components/User"
 
+
+// const isLoggedIn = false;
+// const name = "Eray"; 
+// let surname = "Aybek";
+// const fullName = "Eray Aybek"
+
+const user = {
+
+}
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+    <div>
+      <User 
+        // name = "Eray"
+        // city = "İstanbul"
+        // age = {23}
+
+
+
+        title = "Başlık"
+        data={{  
+          name: 'Eray Aybek',
+          city: 'İstanbul',
+          age: 23,
+          }} 
+          friends = {["Ahmet", "Mehmet", "Ayşe"]}
+      /> 
     </div>
+    
   );
+
 }
 
 export default App;
+
+
+
+      {/*  
+      <div>{ isLoggedIn ? <div>Hoşgeldin {fullName}</div> : <Login /> }</div>
+      
+      <div>
+        { isLoggedIn && <div>Hoşgeldin {fullName} </div> }
+        { !isLoggedIn && <a href="#/">Giriş Yapın</a>}
+      </div>
+
+
+      <div>{name} {surname}</div>
+       
+      
+      <div>
+        <p>
+          test
+        </p>
+      </div>
+
+      <label htmlFor="test">Test</label>
+      {/* <Header /> */} 
